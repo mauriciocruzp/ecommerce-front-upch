da858b49c994c84deb29fc56d0533912ad87d1c7
Sirsen <213473@ids.upchiapas.edu.mx>
2022-11-04 11:17:31 -0600

    new product view created

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import NewProduct from './containers/NewProduct/NewProduct'
 import './index.css'
 import SignUp from './pages/SignUp/SignUp'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignUp />
   </React.StrictMode>
 )
