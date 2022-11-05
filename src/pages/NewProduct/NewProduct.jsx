e15717c2719b550c4e3de0f9a4bd6a872c537fb5
mauriciocruzp <213352@ids.upchiapas.edu.mx>
2022-11-04 22:34:57 -0600

    adjust new product view

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -1,6 +1,7 @@
 import Button from "../../components/Button/Button";
+import NewProductInput from "../../components/Input/NewProductInput";
+import NavBar from "../../containers/NavBar/NavBar";
 import Input from "../../components/Input/Input";
-import NavBar from "../NavBar/NavBar";
 import "./NewProduct.css"
 
 
@@ -9,42 +10,48 @@ function NewProduct(props) {
     <>
       <NavBar />
       <div className="new-product">
-        <h1>Agregar nuevo producto</h1>
         <div className="np-container">
-          <div className="product-details">
-            <div className="img-product-div">
-              <img src={props.img} alt={props.productName} />
-            </div>
-            <Button text="Agregar imagen" />
-            <div className="product-category">
-              <label>Categoría
-                <br />
-                <select>
-                  <option value="">Categoría</option>
-                  <option value="">Tecnología</option>
-                  <option value="">Alimentos</option>
-                  <option value="">Música</option>
-                </select>
-              </label>
-            </div>
-            <Button classNameButton="danger-button" text="Cancelar" />
+          <div className="np-head">
+            <h1>Agregar nuevo producto</h1>
           </div>
-          <div className="product-info">
-            <div className="product-name">
-              <Input text="Nuevo producto:" type="text" />
+          <div className="product-details">
+            <div className="cont-left">
+              <div className="img-product-div">
+                <img src={props.img} alt={props.productName} />
+              </div>
+              <Button classNameButton='primary-button' text="Agregar imagen" />
+              <div className="product-category">
+                <label>Categoría
+                  <br />
+                  <select>
+                    <option value="">Categoría</option>
+                    <option value="">Tecnología</option>
+                    <option value="">Alimentos</option>
+                    <option value="">Música</option>
+                  </select>
+                </label>
+              </div>
+              <Button classNameButton="danger-button" text="Cancelar" />
             </div>
-            <div className="details">
-              <Input text="Descripción" type="text" />
-            </div>
-            <div className="inventory">
-              <Input text="Cantidad de inventario" type="number" />
-            </div>
-            <div className="price">
-              <Input text="Precio" type="text" />
-            </div>
-            <div className="upload-confirmation-product">
-              <Input type="checkbox" text="Al publicar un producto, acepto los términos y condiciones de uso del servicio proporcionado" />
-              <Button classNameButton="primary-button" text="Subir producto" />
+            <div className="cont-right">
+              <div className="product-info">
+                <div className="product-name">
+                  <NewProductInput text="Nombre del producto:" width='680px' height='32px' type="text" />
+                </div>
+                <div className="details">
+                  <NewProductInput text="Descripción:" width='680px' height='120px' type="text" />
+                </div>
+                <div className="inventory">
+                  <Input text="Cantidad de inventario" type="number" />
+                </div>
+                <div className="price">
+                  <NewProductInput text="Precio" width='51px' height='32px' type="text" />
+                </div>
+                <div className="upload-confirmation-product">
+                  <Input type="checkbox" text="Al publicar un producto, acepto los términos y condiciones de uso del servicio proporcionado" />
+                  <Button classNameButton="primary-button" text="Subir producto" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
