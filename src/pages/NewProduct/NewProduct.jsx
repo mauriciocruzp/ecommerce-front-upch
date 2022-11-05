aceee84bc8b7302f5651d26a174909cfefd0919e
mauriciocruzp <213352@ids.upchiapas.edu.mx>
2022-11-04 19:55:08 -0600

    s

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -8,47 +8,47 @@ function NewProduct(props) {
   return (
     <>
       <NavBar />
-       <div className="new-product">
+      <div className="new-product">
         <h1>Agregar nuevo producto</h1>
-          <div className="np-container">
-            <div className="product-details">
-              <div className="img-product-div">
-                <img src={props.img} alt={props.productName} />
-              </div>
-              <Button text="Agregar imagen" />
-              <div className="product-category">
-                <label>Categoría
-                  <br />
-                  <select>
-                    <option value="">Categoría</option>
-                    <option value="">Tecnología</option>
-                    <option value="">Alimentos</option>
-                    <option value="">Música</option>
-                  </select>
-                </label>
-              </div>
-              <Button classNameButton="danger-button" text="Cancelar" />
+        <div className="np-container">
+          <div className="product-details">
+            <div className="img-product-div">
+              <img src={props.img} alt={props.productName} />
             </div>
-            <div className="product-info">
-              <div className="product-name">
-                <Input text="Nuevo producto:" type="text" />
-              </div>
-              <div className="details">
-                <Input text="Descripción" type="text" />
-              </div>
-              <div className="inventory">
-                <Input text="Cantidad de inventario" type="number" />
-              </div>
-              <div className="price">
-                <Input text="Precio" type="text" />
-              </div>
-              <div className="upload-confirmation-product">
-                <Input type="checkbox" text="Al publicar un producto, acepto los términos y condiciones de uso del servicio proporcionado" />
-                <Button classNameButton="primary-button" text="Subir producto" />
-              </div>
+            <Button text="Agregar imagen" />
+            <div className="product-category">
+              <label>Categoría
+                <br />
+                <select>
+                  <option value="">Categoría</option>
+                  <option value="">Tecnología</option>
+                  <option value="">Alimentos</option>
+                  <option value="">Música</option>
+                </select>
+              </label>
+            </div>
+            <Button classNameButton="danger-button" text="Cancelar" />
+          </div>
+          <div className="product-info">
+            <div className="product-name">
+              <Input text="Nuevo producto:" type="text" />
+            </div>
+            <div className="details">
+              <Input text="Descripción" type="text" />
+            </div>
+            <div className="inventory">
+              <Input text="Cantidad de inventario" type="number" />
+            </div>
+            <div className="price">
+              <Input text="Precio" type="text" />
+            </div>
+            <div className="upload-confirmation-product">
+              <Input type="checkbox" text="Al publicar un producto, acepto los términos y condiciones de uso del servicio proporcionado" />
+              <Button classNameButton="primary-button" text="Subir producto" />
             </div>
           </div>
-       </div>
+        </div>
+      </div>
     </>
   )
 }
