1dc692b20c5ddff357b43b16a5bdee5bb171db47
edualt <eduardosebastianhdz503@gmail.com>
2022-11-07 15:15:34 -0600

    feat(app): add routes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUp from './SignUp/SignUp';
-import Home from './Home/Home';
-import LogIn from './LogIn/LogIn';
-import ProductDetail from './ProductDetail/ProductDetail';
-import NewProduct from './NewProduct/NewProduct';
-import Admin from './Admin/Admin';
-import RoleContext from '../context/RoleContext';
-import AuthProvider from '../context/AuthContext/AuthProvider';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthProvider from "../context/AuthContext/AuthProvider";
+import RequireAuth from "../utils/RequireAuth";
+import Admin from "./Admin/Admin";
+import Home from "./Home/Home";
+import LogIn from "./LogIn/LogIn";
+import NewProduct from "./NewProduct/NewProduct";
+import ProductDetail from "./ProductDetail/ProductDetail";
+import SignUp from "./SignUp/SignUp";
 
 function App() {
   return (
@@ -14,22 +14,31 @@ function App() {
       <BrowserRouter>
         <AuthProvider>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/register' element={<SignUp />} />
-            <Route path='/login' element={<LogIn />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
             <Route
-              path='/details'
+              path="/product/:id"
               element={
-                <ProductDetail
-                  productName='Memoria RAM DDR4 8GB 2933MHz Kingston HyperX Predator RGB 1 Modulo Negro / HX429C15PB3A/8'
-                  img='https://ddtech.mx/assets/uploads/f1cc55f716f02f52f65f99080f4361ef.jpg'
-                  price='1,249.00'
-                  details='Memoria RAM DDR4 8GB 2933MHz Kingston HyperX Predator RGB 1 Modulo Negro / HX429C15PB3A/8'
-                />
+                <ProductDetail />
               }
             />
-            <Route path='/new-product' element={<NewProduct />} />
-            <Route path='/admin' element={<Admin />} />
+            <Route
+              path="/new-product"
+              element={
+                <RequireAuth>
+                  <NewProduct />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <RequireAuth>
+                  <Admin />
+                </RequireAuth>
+              }
+            />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
