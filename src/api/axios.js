61792ad1f996ae391f56e037976998268db8b6bc
edualt <eduardosebastianhdz503@gmail.com>
2022-11-07 14:43:02 -0600

    fix(axios): change baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-    baseURL: "http://ec2-54-224-50-121.compute-1.amazonaws.com/api/",
+export const axiosInstance = axios.create({
+    baseURL: "http://ec2-34-233-135-46.compute-1.amazonaws.com/api",
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
     },
 });
-
-export default axiosInstance;
