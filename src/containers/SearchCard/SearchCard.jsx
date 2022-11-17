4bbd44209923588c8c13cc6b67e1dd8ed37bc61f
mauriciocruzp <213352@ids.upchiapas.edu.mx>
2022-11-16 20:13:58 -0600

    create search page

--- src/containers/SearchCard/SearchCard.jsx
+++ src/containers/SearchCard/SearchCard.jsx
@@ -1,16 +1,21 @@
+import NavBar from "../NavBar/NavBar";
+
 function SearchCard(props) {
   return (
-    <div className="w-2/3 h-44 flex flex-row">
-      <div className="w-1/3 h-full bg-gray-300">
-        <img className="w-full h-full" src={props.img} alt="" />
-      </div>
-      <div className="flex flex-col p-3 w-full items-start">
-        <div className="w-full h-1/2">
-          <h2>Nombre del producto</h2>
+    <>
+      {/* <NavBar /> */}
+      <div className="w-2/3 h-44 flex flex-row mb-5">
+        <div className="w-2/4 h-full bg-gray-300">
+          <img className="w-full h-full" src={props.img} alt="" />
         </div>
+        <div className="flex flex-col px-5 py-3 w-full items-start">
+          <div className="w-full h-1/2">
+            <h2>Nombre del producto</h2>
+          </div>
           <h3 className="text-violet-500">$23,000.00</h3>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -4,13 +4,21 @@ import SearchCard from "../../containers/SearchCard/SearchCard";
 function SearchPage() {
   return (
     <>
-    <div className="w-full ">
-      <div className="w-full">
-        <div><h1>Producto buscado</h1></div>
-        <div><p></p></div>
+      <div className="w-full ">
+        <div className="w-full mt-6 pl-8">
+          <div><h1>Producto buscado</h1></div>
+          <div><p>34 resultados</p></div>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="w-3/4 bg-white p-5 rounded mt-9 flex flex-col">
+            <SearchCard />
+            <SearchCard />
+            <SearchCard />
+            <SearchCard />
+
+          </div>
+        </div>
       </div>
-      <SearchCard />
-    </div>
     </>
   );
 }
