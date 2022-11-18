3a2701a2d415dad8b4a47ea1f0f01cde75bbaa3e
edualt <eduardosebastianhdz503@gmail.com>
2022-11-18 11:36:01 -0600

    style(HomeBanner): change design

--- src/components/Banner/HomeBanner.jsx
+++ src/components/Banner/HomeBanner.jsx
@@ -11,18 +11,14 @@ export default function HomeBanner() {
 
   return (
     <>
-      <div className="banner-background w-full h-96 flex flex-col justify-center items-center">
-        {/* make a div with 2 columns */}
-        <div className="w-2/3 h-2/3 flex flex-row justify-between items-center">
-            {/* left column */}
-            <div className="h-full flex flex-col justify-center">
+      <div className="banner-background w-full flex flex-col justify-center items-center">
+        <div className="w-3/4 h-2/3 flex flex-row justify-between items-center">
+            <div className="w-2/3 h-full flex flex-col justify-center">
                 <p className="pb-2 text-4xl text-white">¡Bienvenido<span className="text-white font-bold">{isAuthenticated ? " " + username : ""}</span>!</p>
                 <p className="pt-2 text-3xl text-white">Aprovecha hoy! Encuentra tus productos favoritos a un precio espectacular</p>
             </div>
-            {/* right column */}
-            <div className="w-1/2 h-full flex flex-col justify-center items-center">
-                {/* dessapear image on mobile */}
-                <img className="hidden md:block" src={homeimg} alt="homeimg" />
+            <div className="w-2/5 flex flex-col justify-center items-center">
+                <img className="hidden md:block banner-image" src={homeimg} alt="homeimg" />
             </div>
         </div>
       </div>
