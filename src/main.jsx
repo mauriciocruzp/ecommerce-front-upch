a837bb4fc91d4bf21cd16a91d3a4edaeee9a71e5
mauriciocruzp <213352@ids.upchiapas.edu.mx>
2022-11-17 21:58:57 -0600

    restore app

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './pages/App'
-import SearchPage from './pages/SearchPage/SearchPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <SearchPage />
+    <App />
   </React.StrictMode>
 )
