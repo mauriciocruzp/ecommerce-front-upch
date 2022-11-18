0cff5407b4b6b207f3d8cf5684cad6df4f45cd0a
Fernando Guerrero <devrrior@gmail.com>
2022-11-18 00:56:21 -0600

    feat(tailwind.config): add input-purple

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,8 @@ module.exports = {
         'light-purple': '#8C60E4',
         'purple-white': '#DDD0F7',
         'light-white': '#C1A9F0',
-        'intermidiate': '#A887EB'
+        'intermidiate': '#A887EB',
+        'input-purple': '#E8DFFA',
       },
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
