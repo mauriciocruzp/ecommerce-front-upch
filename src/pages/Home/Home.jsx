3392c1d42f7d091138354999f3e7fb913f5101c2
edualt <eduardosebastianhdz503@gmail.com>
2022-11-17 21:50:45 -0600

    feat(home): add home banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import './Home.css'
-import NavBar from '../../containers/NavBar/NavBar'
+import HomeBanner from '../../components/Banner/HomeBanner.jsx';
 import CardsContainer from '../../containers/CardsContainer/CardsContainer';
-
+import NavBar from '../../containers/NavBar/NavBar';
+import './Home.css';
 
 function Home() {
   return (
     <>
       <NavBar />
       <div className='home'>
+        <HomeBanner />
         <div className="cards">
           <CardsContainer />
         </div>
