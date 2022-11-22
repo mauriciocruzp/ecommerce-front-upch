45dde98040d11ecfd67b854949871a85f3ac638b
mauriciocruzp <213352@ids.upchiapas.edu.mx>
2022-11-22 09:27:36 -0600

    delete buttons from number input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,7 +2,7 @@ function Input({type, name, text, placeholder, id}) {
   return (
     <div className='flex flex-col'>
       <label className='mt-3' htmlFor={id}>{text}</label>
-      <input type={type} name={name} id={id} placeholder={placeholder} className='bg-purple-white rounded-md px-3 py-1 placeholder-intermidiate focus:outline-none focus:outline-principal-purple' />
+      <input type={type} name={name} id={id} placeholder={placeholder} className='bg-purple-white rounded-md px-3 py-1 placeholder-intermidiate focus:outline-none focus:outline-principal-purple but' />
     </div>
   );
 }
