6b67f392af22b2fc403ecf912dba87a624555c37
Fernando Guerrero <devrrior@gmail.com>
2022-11-24 01:14:50 -0600

    fix(pages/ProductForm): update product form

--- src/pages/ProductForm/ProductForm.jsx
+++ src/pages/ProductForm/ProductForm.jsx
@@ -6,19 +6,35 @@ import * as Yup from 'yup';
 import { createProduct } from '../../api/services/product';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
-import NewProductInput from '../../components/Input/NewProductInput';
 import Textarea from '../../components/Input/Textarea';
 import NavBar from '../../containers/NavBar/NavBar';
 import useAuth from '../../hooks/useAuth';
 import HomeSvg from '../../assets/svg/home.svg';
 import ProductSvg from '../../assets/svg/product.svg';
 import OrderSvg from '../../assets/svg/order.svg';
+import { getCategories } from '../../api/services/category';
+import { useState } from 'react';
+import Spinner from '../../components/Spinner/Spinner';
+import capitalizeFirstLetter from '../../utils/capitalizeFirstLetter';
+import { uploadFile } from '../../api/services/file';
 
-function ProductForm() {
+const ProductForm = () => {
   const { authState } = useAuth();
-  // const { axiosInstance } = useAxiosPrivate();
+
+  const [categories, setCategories] = useState(null);
+
+  const [filename, setFilename] = useState(null);
+
   useEffect(() => {
     console.log(authState.accessToken);
+
+    const fetchCategories = async () => {
+      const response = await getCategories();
+
+      setCategories(response.data.data);
+    };
+
+    fetchCategories();
   }, []);
 
   const navigate = useNavigate();
@@ -26,56 +42,69 @@ function ProductForm() {
   const initialValues = {
     title: '',
     description: '',
-    image: null,
+    image: '',
     stock: '',
     price: '',
-    categoryIds: '',
+    categoryIds: [],
   };
 
   const validationSchema = Yup.object({
-    title: Yup.string().required('Campo requerido'),
-    description: Yup.string().required('Campo requerido'),
-    image: Yup.string().required('Campo requerido'),
-    stock: Yup.string().required('Campo requerido'),
-    price: Yup.string().required('Campo requerido'),
-    categoryIds: Yup.string().required('Campo requerido'),
+    title: Yup.string().required('Titulo requerido'),
+    description: Yup.string().required('Descripción requerida'),
+    image: Yup.string().required('Imagen requerida'),
+    stock: Yup.string().required('Cantidad de inventario requerida'),
+    price: Yup.string().required('Precio requerido'),
+    categoryIds: Yup.array().min(1, 'Debe seleccionar al menos una categoría'),
   });
 
-  async function handleSubmit(values) {
-    values.categoryIds = 1;
-
+  const handleSubmit = async (values) => {
     const formData = new FormData();
-    formData.append('title', values.title);
-    formData.append('description', values.description);
-    formData.append('image', values.image);
-    formData.append('stock', values.stock);
-    formData.append('price', values.price);
-    formData.append('categoryIds', values.categoryIds);
+    formData.append('file', values.image);
 
-    console.log(values);
+    const fileResponse = await uploadFile(formData);
 
-    const response = await createProduct(formData);
+    if (fileResponse.status !== 201) return alert('Error al subir la imagen');
+
+    const response = await createProduct(
+      values.title,
+      values.description,
+      values.price,
+      values.stock,
+      fileResponse.data.data.url,
+      values.categoryIds
+    );
 
     if (response.status === 201) {
       alert('Producto creado exitosamente');
       navigate('/');
       return;
     }
-  }
+  };
+
+  const renderCategories = () =>
+    categories.map((category) => (
+      <option key={category.id} value={category.id}>
+        {capitalizeFirstLetter(category.name)}
+      </option>
+    ));
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        setFieldValue,
-      }) => (
-        <div className='flex flex-col h-screen'>
-          <NavBar />
+    <>
+      <NavBar />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          setFieldValue,
+        }) => (
           <div className='flex'>
             <div className='w-1/5 pl-8 pt-5 flex flex-col gap-4'>
               <h2>Panel de control</h2>
@@ -95,72 +124,141 @@ function ProductForm() {
               </ul>
             </div>
             <div className='w-4/5 pl-32 py-5 bg-white'>
-              <div className='w-2/3 flex flex-col gap-10'>
-                <h1>Agregar producto nuevo</h1>
-                <div className='flex flex-col gap-8'>
-                  <div className='flex flex-col gap-2'>
-                    <label>Titulo:</label>
-                    <Input type='text' placeholder='Microfono Shure' />
-                  </div>
-                  <div className='flex flex-col gap-2'>
-                    <label>Description:</label>
-                    <Textarea placeholder='Microfono de alta calidad...'></Textarea>
-                  </div>
-                  <div className='flex flex-col gap-2'>
-                    <label>Precio:</label>
-                    <Input type='number' placeholder='$1,000' />
-                  </div>
-                  <div className='flex flex-col gap-2'>
-                    <label>Cantidad de inventario:</label>
-                    <Input type='number' placeholder='3' />
-                  </div>
-                  <div className='flex flex-col gap-2'>
-                    <label>Category:</label>
-                    <select
-                      multiple
-                      id='countries_multiple'
-                      class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-principal-purple block w-full p-2.5'
-                    >
-                      <option selected>Choose countries</option>
-                      <option value='US'>United States</option>
-                      <option value='CA'>Canada</option>
-                      <option value='FR'>France</option>
-                      <option value='DE'>Germany</option>
-                    </select>
-                  </div>
-                  <div className='flex flex-col gap-2'>
-                    <label>Imagen del producto:</label>
+              <h1>Agregar producto nuevo</h1>
+              <form onSubmit={handleSubmit}>
+                <div className='w-2/3 flex flex-col gap-10 mt-5'>
+                  <div className='flex flex-col gap-8'>
+                    <div className='flex flex-col gap-2'>
+                      <label>Titulo:</label>
+                      <Input
+                        type='text'
+                        value={values.title}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        placeholder='Microfono Shure'
+                        name='title'
+                      />
+                      {errors.title && touched.title && (
+                        <p className='text-red-500'>{errors.title}</p>
+                      )}
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label>Description:</label>
+                      <Textarea
+                        value={values.description}
+                        name='description'
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        placeholder='Microfono de alta calidad...'
+                      ></Textarea>
+                      {errors.description && touched.description && (
+                        <p className='text-red-500'>{errors.description}</p>
+                      )}
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label>Precio:</label>
+                      <Input
+                        type='number'
+                        value={values.price}
+                        name='price'
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        placeholder='$1,000'
+                      />
+                      {errors.price && touched.price && (
+                        <p className='text-red-500'>{errors.price}</p>
+                      )}
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label>Cantidad de inventario:</label>
+                      <Input
+                        type='number'
+                        value={values.stock}
+                        name='stock'
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        placeholder='3'
+                      />
+                      {errors.stock && touched.stock && (
+                        <p className='text-red-500'>{errors.stock}</p>
+                      )}
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label>Category:</label>
+                      {categories ? (
+                        <select
+                          value={values.categoryIds}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          multiple
+                          name='categoryIds'
+                          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-principal-purple block w-full p-2.5'
+                        >
+                          {renderCategories()}
+                        </select>
+                      ) : (
+                        <Spinner />
+                      )}
+                      {errors.categoryIds && touched.categoryIds && (
+                        <p className='text-red-500'>{errors.categoryIds}</p>
+                      )}
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label>Imagen del producto:</label>
 
-                    <div class='flex w-2/5'>
-                      <label
-                        for='dropzone-file'
-                        class='flex flex-col items-center justify-center w-full px-6 py-4 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-purple-100'
-                      >
-                        <div class='flex items-center justify-center gap-6'>
-                          <img src={ImgSvg} />
-                          <div className='flex flex-col'>
-                            <p class='text-sm font-semibold text-gray-500 dark:text-gray-400'>
-                              Sube una imagen
-                            </p>
-                            <p class='text-xs text-gray-500 dark:text-gray-400'>
-                              Seleccionala o arrastrala
-                            </p>
+                      <div className='flex w-fit'>
+                        <label
+                          htmlFor='dropzone-file'
+                          className='flex flex-col items-center justify-center w-full px-6 py-4 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-purple-100'
+                        >
+                          <div className='flex items-center justify-center gap-6'>
+                            <img src={ImgSvg} />
+                            <div className='flex flex-col'>
+                              <p className='text-sm font-semibold text-gray-500 dark:text-gray-400'>
+                                Sube una imagen
+                              </p>
+                              {!filename ? (
+                                <p className='text-xs text-gray-500 dark:text-gray-400'>
+                                  seleccionala o arrastrala
+                                </p>
+                              ) : (
+                                <p className='text-xs text-gray-500 dark:text-gray-400'>
+                                  {filename}
+                                </p>
+                              )}
+                            </div>
                           </div>
-                        </div>
-                        <input id='dropzone-file' type='file' class='hidden' />
-                      </label>
+                          <input
+                            id='dropzone-file'
+                            name='image'
+                            type='file'
+                            className='hidden'
+                            onChange={(event) => {
+                              setFieldValue(
+                                'image',
+                                event.currentTarget.files[0]
+                              );
+                              setFilename(event.currentTarget.files[0].name);
+                            }}
+                            onBlur={handleBlur}
+                          />
+                        </label>
+                      </div>
+                      {errors.image && (
+                        <p className='text-red-500'>{errors.image}</p>
+                      )}
                     </div>
                   </div>
+                  <div>
+                    <Button type='submit'>Enviar</Button>
+                  </div>
                 </div>
-                <div>
-                  <Button>Enviar</Button>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
-        </div>
-      )}
-    </Formik>
+        )}
+      </Formik>
+    </>
   );
-}
+};
 export default ProductForm;
