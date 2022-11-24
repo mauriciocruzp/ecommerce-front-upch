05cc6a8dfdd9d3194bde5465cad73adf73b79a52
Fernando Guerrero <devrrior@gmail.com>
2022-11-24 01:13:54 -0600

    fix(component/Input): update input props

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,23 @@
-function Input({type, name, placeholder, id}) {
+function Input({
+  type,
+  placeholder,
+  id,
+  name,
+  value,
+  handleChange,
+  handleBlur,
+}) {
   return (
-    <input type={type} name={name} id={id} placeholder={placeholder} className='bg-gray-50 rounded-md px-3 py-1 placeholder-gray-400 focus:outline-none focus:outline-principal-purple' />
+    <input
+      value={value}
+      type={type}
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      className='bg-gray-50 rounded-md px-3 py-1 placeholder-gray-400 focus:outline-none focus:outline-principal-purple'
+    />
   );
 }
 
