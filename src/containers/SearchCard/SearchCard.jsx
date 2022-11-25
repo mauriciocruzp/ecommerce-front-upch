68b80fa6941ee18f00ba9cb67775d557680f7cd2
mauriciocruzp <213352@ids.upchiapas.edu.mx>
2022-11-24 22:29:38 -0600

    improve search page

--- src/containers/SearchCard/SearchCard.jsx
+++ src/containers/SearchCard/SearchCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function SearchCard({ product }) {
   return (
     <>
-      {/* <Link to={`product/${product.id}`}> */}
+      <Link to={`/product/${product.id}`}>
       <div className="w-2/3 h-48 flex flex-row mb-5">
         <div className="w-2/4 h-full bg-gray-300">
           <img className="w-full h-full" src={product.imageUrl} alt={product.title} />
@@ -15,7 +15,7 @@ function SearchCard({ product }) {
           <h3 className="text-light-purple">{'$' + product.price}</h3>
         </div>
       </div>
-      {/* </Link> */}
+      </Link>
     </>
   );
 }
