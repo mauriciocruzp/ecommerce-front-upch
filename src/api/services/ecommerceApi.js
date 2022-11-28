484cb0e6d4b788e98a825a53f094227c276c9e17
Fernando Guerrero <devrrior@gmail.com>
2022-11-27 23:06:27 -0600

    feat(api/services/ecommerceApi.js): add getCategories and getProductStatus

--- src/api/services/ecommerceApi.js
+++ src/api/services/ecommerceApi.js
@@ -1,21 +1,43 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const BASE_URL = 'http://127.0.0.1:8080/api';
 
 // Define a service using a base URL and expected endpoints
 export const ecommerceApi = createApi({
   reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: (headers) => {
+      const accessToken = localStorage.getItem('accessToken');
+
+      if (accessToken) {
+        headers.set('Authorization', `Bearer ${accessToken}`);
+      }
+
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getProducts: builder.query({
-        query: () => '/product',
+      query: () => '/product',
     }),
     getProductById: builder.query({
-        query: (id) => `/product/${id}`,
+      query: (id) => `/product/${id}`,
+    }),
+    getCategories: builder.query({
+      query: () => '/category',
+    }),
+    getProductStatus: builder.query({
+      query: () => '/product-status',
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetProductsQuery, useGetProductByIdQuery } = ecommerceApi;
+export const {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+  useGetCategoriesQuery,
+  useGetProductStatusQuery,
+} = ecommerceApi;
