8c8331a992aa397edb28f7a0389541afc90b9bbd
Fernando Guerrero <devrrior@gmail.com>
2022-12-02 19:46:29 -0600

    feat(Navbar): add dropdown to profile section and add links

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -1,7 +1,9 @@
+import { Menu, Transition } from '@headlessui/react';
 import SearchSvg from '../../assets/svg/search.svg';
 import CartSvg from '../../assets/svg/cart.svg';
 import AvatarSvg from '../../assets/svg/avatar.svg';
 import { Link, useNavigate } from 'react-router-dom';
+import { Fragment } from 'react';
 
 function NavBar() {
   const navigate = useNavigate();
@@ -34,9 +36,63 @@ function NavBar() {
         >
           <img src={CartSvg} className='w-3.5 h-3.5' />
         </Link>
-        <div className='w-8 h-8'>
-          <img src={AvatarSvg} className='w-full h-full' draggable={false} />
-        </div>
+        <Menu as='div'>
+          <Menu.Button className='w-full h-8 flex items-center'>
+            <img src={AvatarSvg} className='w-full h-full' draggable={false} />
+            <svg
+              className='-mr-1 ml-2 h-5 w-5'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              aria-hidden='true'
+            >
+              <path
+                fillRule='evenodd'
+                d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter='transition ease-out duration-100'
+            enterFrom='transform opacity-0 scale-95'
+            enterTo='transform opacity-100 scale-100'
+            leave='transition ease-in duration-75'
+            leaveFrom='transform opacity-100 scale-100'
+            leaveTo='transform opacity-0 scale-95'
+          >
+            <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+              <div
+                role='menu'
+                aria-orientation='vertical'
+                aria-labelledby='menu-button'
+                tabIndex='-1'
+              >
+                <div className='py-1' role='none'>
+                  <Link
+                    to='/'
+                    className='text-gray-700 block px-4 py-2 text-sm'
+                    role='menuitem'
+                    tabIndex='-1'
+                    id='menu-item-0'
+                  >
+                    Mis direcciones
+                  </Link>
+                  <Link
+                    to='/admin'
+                    className='text-gray-700 block px-4 py-2 text-sm'
+                    role='menuitem'
+                    tabIndex='-1'
+                    id='menu-item-1'
+                  >
+                    Panel de administración
+                  </Link>
+                </div>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
     </div>
   );
