45fcc603b7736e02438b4b8d1e4ccf9d3c344376
Fernando Guerrero <devrrior@gmail.com>
2022-12-06 00:44:43 -0600

    feat(Navbar): hide some elments depend on user

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -7,10 +7,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { update } from '../../reducers/keywordReducer';
 import { Fragment } from 'react';
 import routes from '../../consts/routes';
+import useAuth from '../../hooks/useAuth';
 
 function NavBar() {
+  const { authState, logout } = useAuth();
 
-  const keyword = useSelector(state => state.keyword.value);
+  const keyword = useSelector((state) => state.keyword.value);
+
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
@@ -45,63 +48,81 @@ function NavBar() {
         >
           <img src={CartSvg} className='w-3.5 h-3.5' />
         </Link>
-        <Menu as='div'>
-          <Menu.Button className='w-full h-8 flex items-center'>
-            <img src={AvatarSvg} className='w-full h-full' draggable={false} />
-            <svg
-              className='-mr-1 ml-2 h-5 w-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fillRule='evenodd'
-                d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
-                clipRule='evenodd'
+        {authState.isAuthenticated && (
+          <Menu as='div'>
+            <Menu.Button className='w-full h-8 flex items-center'>
+              <img
+                src={AvatarSvg}
+                className='w-full h-full'
+                draggable={false}
               />
-            </svg>
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter='transition ease-out duration-100'
-            enterFrom='transform opacity-0 scale-95'
-            enterTo='transform opacity-100 scale-100'
-            leave='transition ease-in duration-75'
-            leaveFrom='transform opacity-100 scale-100'
-            leaveTo='transform opacity-0 scale-95'
-          >
-            <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-              <div
-                role='menu'
-                aria-orientation='vertical'
-                aria-labelledby='menu-button'
-                tabIndex='-1'
+              <svg
+                className='-mr-1 ml-2 h-5 w-5'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                aria-hidden='true'
               >
-                <div className='py-1' role='none'>
-                  <Link
-                    to={routes.addressList}
-                    className='text-gray-700 block px-4 py-2 text-sm'
-                    role='menuitem'
-                    tabIndex='-1'
-                    id='menu-item-0'
-                  >
-                    Mis direcciones
-                  </Link>
-                  <Link
-                    to={routes.admin}
-                    className='text-gray-700 block px-4 py-2 text-sm'
-                    role='menuitem'
-                    tabIndex='-1'
-                    id='menu-item-1'
-                  >
-                    Panel de administración
-                  </Link>
+                <path
+                  fillRule='evenodd'
+                  d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                <div
+                  role='menu'
+                  aria-orientation='vertical'
+                  aria-labelledby='menu-button'
+                  tabIndex='-1'
+                >
+                  <div className='py-1' role='none'>
+                    <Link
+                      to={routes.addressList}
+                      className='text-gray-700 block px-4 py-2 text-sm'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='menu-item-0'
+                    >
+                      Mis direcciones
+                    </Link>
+                    {authState.user.roles.includes('ROLE_ADMIN') && (
+                      <Link
+                        to={routes.admin}
+                        className='text-gray-700 block px-4 py-2 text-sm'
+                        role='menuitem'
+                        tabIndex='-1'
+                        id='menu-item-1'
+                      >
+                        Panel de administración
+                      </Link>
+                    )}
+                    <Link
+                      to={routes.login}
+                      onClick={() => logout()}
+                      className='text-red-500 block px-4 py-2 text-sm'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='menu-item-0'
+                    >
+                      Cerrar sesión
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        )}
       </div>
     </div>
   );
