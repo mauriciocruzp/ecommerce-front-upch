e56f8a7eff65997aedb0034e8d7b6c6f22c5fdb3
Mauricio Cruz <mawcp7@gmail.com>
2022-12-06 10:48:46 -0600

    add hr

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -34,12 +34,14 @@ function Checkout() {
                 </form>
               </div>
               <div className="w-full mt-14 flex justify-end">
-                <Button width='w-3/12'>Continuar</Button>
+                <Button width='w-3/12'>Pagar</Button>
               </div>
             </div>
             <div className="w-5/12 flex justify-end">
-              <div className="w-4/5 bg-gray-custom rounded p-6">
-                <h2>Resumen de compra</h2>
+              <div className="w-4/5 bg-white rounded p-6">
+                <p className="text-xl font-semibold">Resumen de compra</p>
+                <hr className="bg-gray-300 my-2" />
+                <p className="text-lg mx-6 mt-6">Productos {2}</p>
               </div>
             </div>
           </div>
