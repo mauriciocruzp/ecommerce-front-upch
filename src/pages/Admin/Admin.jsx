b760be91d006266dc86a624573b537320fe7fdf0
Fernando Guerrero <devrrior@gmail.com>
2022-12-06 19:18:44 -0600

    feat(admin): add message for next features

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -7,43 +7,30 @@ import Button from '../../components/Button/Button';
 import StatsCard from '../../containers/Card/StatsCard';
 import OrderCard from '../../containers/Card/OrderCard';
 import routes from '../../consts/routes';
+import AdminSideBar from '../../components/AdminSideBar/AdminSideBar';
 
 function Admin() {
-
   return (
-    <>
-    <NavBar />
-    <div className='flex bg-white'>
-      <div className='bg-gray-200 w-1/5 pl-8 pt-5 flex flex-col gap-4'>
-        <h2>Panel de control</h2>
-        <ul className='flex flex-col gap-4'>
-          <div className='flex gap-2'>
-              {/* <img src={HomeSvg} className='w-4' /> */}
-              <li>Home</li>
+    <div className='h-screen'>
+      <NavBar />
+      <div className='flex h-full'>
+        <AdminSideBar />
+        <div className='w-4/5 px-32 py-5 bg-white flex flex-col gap-8'>
+          <div className='flex w-full justify-between items-center'>
+            <h1>Reporte de ventas</h1>
+            <Link to={routes.productNew}>
+              <Button type='submit'>Nuevo producto</Button>
+            </Link>
           </div>
-          <div className='flex gap-2'>
-            {/* <img src={ProductSvg} className='w-4' /> */}
-            <li>Productos</li>
+
+          <div className='flex justify-between flex-wrap h-4/5'>
+            <h5>
+              Proximamente se mostrará estadisticas de la tienda, gracias!
+            </h5>
           </div>
-          <div className='flex gap-2'>
-            {/* <img src={OrderSvg} className='w-4' /> */}
-                <li>Ordenes</li>
-          </div>
-          </ul>
-      </div>
-      <div className='w-4/5 pl-32 py-4 flex flex-row'>
-        {/* Estadísticas */}
-        <div className='flex flex-col sm:space-y-6'>
-          <h1 >Reporte de ventas</h1>
-          <StatsCard />
-          <StatsCard />
         </div>
       </div>
-        <Link to={routes.productNew} className='py-4 pr-12'>
-          <Button type="submit">Nuevo producto</Button>
-        </Link>
     </div>
-    </>
   );
 }
 
